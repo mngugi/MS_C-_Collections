abc29775e95e90ab5c673ab83bebbcd826f49fb3
P.M.Ngugi <mngugi7@hotmail.com>
2025-12-23 12:26:12 +0300

    Create records_example.cs

---
+++ records_example.cs
@@ -0,0 +1,27 @@
+using System;
+
+record Student(string Name, double Score, char Grade);
+
+class Program
+{
+    static void Main()
+    {
+        var students = new[]
+        {
+            new Student("Sophia", 94.6, 'A'),
+            new Student("Nicolas", 83.6, 'B'),
+            new Student("Zahirah", 83.4, 'B'),
+            new Student("Jeong", 95.4, 'A')
+        };
+
+        // Table header
+        Console.WriteLine($"{"Student",-10} {"Score",-6} Grade");
+        Console.WriteLine(new string('-', 25));
+
+        // Table rows
+        foreach (var s in students)
+        {
+            Console.WriteLine($"{s.Name,-10} {s.Score,-6:F1} {s.Grade}");
+        }
+    }
+}
