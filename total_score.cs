1368660078f11c6d132d437333bd23baf01a0c06
P.M.Ngugi <mngugi7@hotmail.com>
2025-12-24 10:54:29 +0300

    Update total_score.cs

--- total_score.cs
+++ total_score.cs
@@ -15,3 +15,11 @@ int currentAssignments = 5;
         int nicolas5 = 85;
 
         double nicolasSum = (nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5);
+
+         int zahirah1 = 84;
+        int zahirah2 = 96;
+        int zahirah3 = 73;
+        int zahirah4 = 85;
+        int zahirah5 = 79;
+
+        double zahirahAverage = (zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5);
