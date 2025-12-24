01be06b2bbe83597bd1fa155098fc429eaae8cff
P.M.Ngugi <mngugi7@hotmail.com>
2025-12-24 10:53:37 +0300

    Update total_score.cs

---
+++ total_score.cs
@@ -0,0 +1,17 @@
+int currentAssignments = 5;
+
+        int sophia1 = 93;
+        int sophia2 = 87;
+        int sophia3 = 98;
+        int sophia4 = 95;
+        int sophia5 = 100;
+
+        double sophiaSum = (sophia1 + sophia2 + sophia3 + sophia4 + sophia5);
+
+         int nicolas1 = 80;
+        int nicolas2 = 83;
+        int nicolas3 = 82;
+        int nicolas4 = 88;
+        int nicolas5 = 85;
+
+        double nicolasSum = (nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5);
