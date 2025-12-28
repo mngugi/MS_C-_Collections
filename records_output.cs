724ad838995c5d5d414b7d2a11c6f105215df594
P.M.Ngugi <mngugi7@hotmail.com>
2025-12-28 19:30:51 +0300

    Update records_output.cs

--- records_output.cs
+++ records_output.cs
@@ -10,10 +10,13 @@ class Program
         Student s1 = new Student(1, "Alice", 12);
         Student s2 = new Student(2, "Brian", 13);
         Student s3 = new Student(3, "Carol", 11);
+        Student s4 = s1 with { Name = "Alicia" }; // Using 'with' expression to create a modified copy of s1
 
         // Output records
         Console.WriteLine(s1);
         Console.WriteLine(s2);
         Console.WriteLine(s3);
+        Console.WriteLine(s4);
+        
     }
 }
